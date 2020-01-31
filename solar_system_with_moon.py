09cf4ea20fed2deb76387b54fcba4fa8543c6706
ChongChong He <he.cc.phy@gmail.com>
2020-01-31 10:01:46 -0500

    add solar_system.py (without moon)

--- solar_system_with_moon.py
+++ solar_system_with_moon.py
@@ -6,11 +6,12 @@ from astropy.time import Time
 from astroquery.jplhorizons import Horizons
 
 sim_start_date = "2018-01-01"     # simulating a solar system starting from this date
-sim_duration = 2 * 365                # (int) simulation duration in days
+sim_duration = 3 * 365                # (int) simulation duration in days
 m_earth = 5.9722e24 / 1.98847e30  # Mass of Earth relative to mass of the sun
 m_moon = 7.3477e22 / 1.98847e30
 
-class Object:                   # define the objects: the Sun, Earth, Mercury, etc
+class Object:
+
     def __init__(self, name, rad, color, r, v):
         self.name = name
         self.r    = np.array(r, dtype=np.float)
@@ -21,29 +22,41 @@ class Object:                   # define the objects: the Sun, Earth, Mercury, e
         self.line, = ax.plot([], [], color=color, linewidth=1.4)
 
 class SolarSystem:
+
     def __init__(self, thesun):
         self.thesun = thesun
         self.planets = []
         self.time = None
         self.timestamp = ax.text(.03, .94, 'Date: ', color='w', transform=ax.transAxes, fontsize='x-large')
+        
     def add_planet(self, planet):
         self.planets.append(planet)
-    def evolve(self):           # evolve the trajectories
+
+    def evolve(self):
         dt = 1.0
         self.time += dt
         plots = []
         lines = []
-        for p in self.planets:
+        for i2, p in enumerate(self.planets):
             p.r += p.v * dt
             acc = -2.959e-4 * p.r / np.sum(p.r**2)**(3./2)  # in units of AU/day^2
+            if p.name == 399:         # Force from the Moon to Earth
+                dr = p.r - self.planets[3].r  # trick here, assuming moon is the 4th object
+                acc += -2.959e-4 * m_moon * dr / np.sum(dr**2)**(3./2)
+            if p.name == 301:         # Force from earth to the Moon
+                dr = p.r - self.planets[2].r
+                acc += -2.959e-4 * m_earth * dr / np.sum(dr**2)**(3./2)
             p.v += acc * dt
             p.xs.append(p.r[0])
             p.ys.append(p.r[1])
             p.plot.set_offsets(p.r[:2])
-            p.line.set_xdata(p.xs)
-            p.line.set_ydata(p.ys)
             plots.append(p.plot)
-            lines.append(p.line)
+            if i2 != 3:          # ignore trajectory lines of the Moon
+                p.line.set_xdata(p.xs)
+                p.line.set_ydata(p.ys)
+                lines.append(p.line)
+        if len(p.xs) > 10000:
+            raise SystemExit("Stopping after a long run to prevent memory overflow")
         self.timestamp.set_text('Date: ' + Time(self.time, format='jd', out_subfmt='date').iso)
         return plots + lines + [self.timestamp]
 
@@ -54,18 +67,20 @@ ax.set_aspect('equal')
 ax.axis('off')
 ss = SolarSystem(Object("Sun", 28, 'red', [0, 0, 0], [0, 0, 0]))
 ss.time = Time(sim_start_date).jd
-colors = ['gray', 'orange', 'blue', 'chocolate']
-sizes = [0.38, 0.95, 1., 0.53]
-names = ['Mercury', 'Venus', 'Earth', 'Mars']
-texty = [.47, .73, 1, 1.5]
-for i, nasaid in enumerate([1, 2, 3, 4]):  # The 1st, 2nd, 3rd, 4th planet in solar system
+colors = ['gray', 'orange', 'blue', 'yellow', 'chocolate']
+sizes = [0.38, 0.95, 1., 0.27, 0.53]
+names = ['Mercury', 'Venus', 'Earth-Moon', 'Earth-Moon', 'Mars']
+for i, nasaid in enumerate([1, 2, 399, 301, 4]):
     obj = Horizons(id=nasaid, location="@sun", epochs=ss.time, id_type='id').vectors()
     ss.add_planet(Object(nasaid, 20 * sizes[i], colors[i], 
                          [np.double(obj[xi]) for xi in ['x', 'y', 'z']], 
                          [np.double(obj[vxi]) for vxi in ['vx', 'vy', 'vz']]))
-    ax.text(0, - (texty[i] + 0.1), names[i], color=colors[i], zorder=1000, ha='center', fontsize='large')
+    ax.text(0, - ([.47, .73, 1, 1.02, 1.5][i] + 0.1),
+            names[2] if i in [2, 3] else names[i], color=colors[i], zorder=1000,
+            ha='center', fontsize='large')
 def animate(i):
     return ss.evolve()
-ani = animation.FuncAnimation(fig, animate, repeat=False, frames=sim_duration, blit=True, interval=20,)
+ani = animation.FuncAnimation(fig, animate, repeat=False, # init_func=init,
+                              frames=sim_duration, blit=True, interval=20,)
 plt.show()
 # ani.save('solar_system_6in_150dpi.mp4', fps=60, dpi=150)
